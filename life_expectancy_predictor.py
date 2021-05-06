260492076471b9a66122f9fa1b0445300172df3c
lau-jr <laurentdeusdedith74@gmail.com>
2021-05-06 21:47:03 +0300

    life expectancy predictor model

---
+++ life_expectancy_predictor.py
@@ -0,0 +1,20 @@
+# TODO: Add import statements
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+bmi_life_model = LinearRegression()
+x_value=bmi_life_data[['BMI']]
+y_value=bmi_life_data[['Life expectancy']]
+bmi_life_model.fit(x_value,y_value)
+
+# Make a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = bmi_life_model.predict([[21.07931]])
+print(laos_life_exp)
